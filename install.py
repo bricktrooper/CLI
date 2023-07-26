53cf2976841433808ce18a8caed9ad1a43bccd6d
bricktrooper <kylepinto1018@gmail.com>
2023-07-26 01:11:07 -0400

    Upgrade flag

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 os.system("git clone https://github.com/bricktrooper/CLI repo")
-os.system("pip install repo/")
+os.system("pip install repo/ --upgrade")
 shutil.rmtree("repo/", ignore_errors = True)
 shutil.rmtree("build", ignore_errors = True)
 shutil.rmtree("dist/", ignore_errors = True)
